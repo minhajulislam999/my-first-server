8c7292b2f4546d1e93cd40c3f145d0299a24c762
minhajul islam <mm01882390860@gmail.com>
2026-04-02 21:28:00 +0600

    initial commit: first node express server

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+
+const users = [
+    {id: 1, name: "Gold", age: 20},
+    {id: 2, name: "Minhaj", age: 22},
+    {id: 3, name: "Rakib", age: 25}
+];
+
+app.get('/users/:id', (req, res) =>{
+    const id = req.params.id;
+    const user = users.find(user => user.id == id);
+    res.json(user);
+})
+
+app.get("/", (req, res) =>{
+    res.json([
+        {name: "Gold", age: 20},
+        {name: "minhaj", age: 220}
+    ])
+})
+
+app.get("/about", (req, res)=>{
+    res.send("This is about page - updated!")
+} )
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
