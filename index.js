a012d9b034b201a72e95cd109fcfe5f16ce757d0
minhajul islam <mm01882390860@gmail.com>
2026-04-05 21:25:30 +0600

    a error fixing

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
 const express = require('express');
-
-
+const cors = require('cors');
 
 const app = express();
-
-const cors = require('cors');
 
 app.use(cors());
 
@@ -16,15 +15,16 @@ const users = [
     {id: 3, name: "Rakib", age: 25}
 ];
 
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
 app.get('/users/:id', (req, res) =>{
     const id = req.params.id;
     const user = users.find(user => user.id == id);
     res.json(user);
 })
-
-app.get('/users', (req, res) => {
-    res.json(users);
-});
 
 app.get("/", (req, res) =>{
     res.json([
@@ -36,6 +36,41 @@ app.get("/", (req, res) =>{
 app.get("/about", (req, res)=>{
     res.send("This is about page - updated!")
 } )
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: '1',
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+let usersCollection;
+async function run() {
+    await client.connect();
+    const db = client.db('mydatabase');
+    usersCollection = db.collection('users');
+    console.log('Connected to MongoDB!');
+
+    
+
+
+}
+
+app.get('/allUsers', async (req, res) => {
+    console.log('db-users route hit!');
+        const users = await usersCollection.find().toArray();
+        res.json(users);
+    });
+
+    app.get('/test', (req, res) => {
+    res.json({message: "test works!"});
+});
+
+
+run();
+
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
